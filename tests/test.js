6c9957afcd5594b68b07dfa4357e28ded568f40d
Ethan <ethan.r.davidson@gmail.com>
2019-06-15 22:36:35 -0700

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -2,8 +2,13 @@
 const MrWorldwide = require('./../index.js');
 
 // Constants
+const [lat, lon] = [33.788301, -117.97106];
+const padding = 0.00001;
 const boundingBoxCoords = {
-
+  minLat: lat - padding,
+  maxLat: lat + padding,
+  minLon: lon - padding,
+  maxLon: lon + padding
 };
 const boundingBoxPixels = {
 
