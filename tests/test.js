20eb4a47bb3579370eeaf0b4c1d5b11f7e2a72ee
Ethan <ethan.r.davidson@gmail.com>
2019-06-15 21:21:01 -0700

    vbnmjhbnmjh

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+// Dependencies
+const MrWorldwide = require('./../index.js');
+
+// Constants
+const boundingBoxCoords = {
+
+};
+const boundingBoxPixels = {
+
+};
+
+// Helpers
+
+// Main Process
+(() => {
+
+})();
