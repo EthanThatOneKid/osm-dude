a84c13db7e0981b3c66991160e983b84ec25c114
Ethan <ethan.r.davidson@gmail.com>
2019-06-16 13:24:16 -0700

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -5,20 +5,17 @@ const MrWorldwide = require('./../index.js');
 // Constants
 const [lat, lon] = [33.788346, -117.971118];
 const padding = 0.000535;
-const boundingBoxCoords = {
-  minLat: lat - padding,
-  maxLat: lat + padding,
-  minLon: lon - padding,
-  maxLon: lon + padding
+const settings = {
+  mapWidth: 1,
+  mapHeight: 1
 };
-const boundingBoxPixels = {w: 1, h: 1};
 
 // Helpers
 
 // Main Process
 (async () => {
 
-  const osm = new MrWorldwide(boundingBoxCoords, boundingBoxPixels);
+  const osm = new MrWorldwide(lat, lon, padding, null, settings);
   await osm.init();
   console.log(osm.getMapData());
 
