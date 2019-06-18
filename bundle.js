b71c414fdc208dbbd83349b07a711215a07f8fff
Ethan <ethan.r.davidson@gmail.com>
2019-06-18 03:58:01 -0700

    made a bundler

--- bundle.js
+++ bundle.js
@@ -1,12 +1,26 @@
 // Dependencies
+const {writeFileSync: w} = require('fs');
 const browserify = require('browserify');
 
+// Helpers
+const streamToString = stream => {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', chunk => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  });
+};
+
 // Main Process
-(() => {
+(async () => {
 
   const b = browserify();
   b.add('./index.js');
+
   const stream = b.bundle();
-  console.log(stream);
+  const result = await streamToString(stream);
+
+  w('./dist/bundle.js', result);
 
 })();
