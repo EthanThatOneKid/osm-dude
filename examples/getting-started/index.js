e3e3119e6827fbf774301202086c910f179a5f08
Ethan <ethan.r.davidson@gmail.com>
2019-07-11 23:55:27 -0700

    created examples folder

---
+++ examples/getting-started/index.js
@@ -0,0 +1,27 @@
+// Dependencies
+const OsmDude = require('./../../index.js');
+/* In Production: */ // const OsmDude = require('osm-dude');
+
+// Constants
+const [lat, lon] = [33.788346, -117.971118];
+const padding = 0.001;
+const settings = {
+  mapWidth: 1,
+  mapHeight: 1
+};
+const tests = [
+  `Initializing OsmDude of bound ${padding}`
+];
+
+// Main Process
+(async () => {
+
+  const osm = new OsmDude(lat, lon, padding, null, settings);
+
+  console.time(tests[0]);
+  await osm.init();
+  console.timeEnd(tests[0]);
+
+  osm.log();
+
+})();
