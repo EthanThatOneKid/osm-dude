9861ccd77b41825b9fad05fe1d107604d27a0b39
Ethan <ethan.r.davidson@gmail.com>
2019-07-11 23:42:12 -0700

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,15 +1,25 @@
 // Dependencies
-const {createWriteStream: w} = require('fs');
+const fs = require('fs');
 const browserify = require('browserify');
 const minify = require('minify-stream');
+
+// Globals
+const {version} = JSON.parse(fs.readFileSync('./package.json'));
+const savePaths = [
+  `./dist/bundle.${version}.js`,
+  './dist/bundle.latest.js'
+];
 
 // Main Process
 (async () => {
 
   const b = browserify();
   b.require('./index.js', {expose: 'osm-dude'});
-  b.bundle()
-    .pipe(minify({sourceMap: false}))
-    .pipe(w('./dist/bundle.js'));
+
+  for (let savePath of savePaths) {
+    b.bundle()
+      .pipe(minify({sourceMap: false}))
+      .pipe(fs.createWriteStream(savePath));
+  }
 
 })();
